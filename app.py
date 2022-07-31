03d5a9899df04755a5e362c01b59b1bb2b3af702
PrasantSingh1729 <singhprasant472@gmail.com>
2022-07-31 15:02:34 +0530

    changed app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ import numpy as np
 
 
 img = ImageSteg()
-
+img.encrypt_text_in_image("test.jpg","IT'S A SECRET MESSAGE","static/")
 res = img.decrypt_text_in_image("static/test_encrypted.png")
 print(res)
